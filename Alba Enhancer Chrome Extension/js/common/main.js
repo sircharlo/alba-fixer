b57966b8a5b7b8f2b47d9eab62b208441eb8d6d3
Charles Olivier Savignac <sircharlo@gmail.com>
2020-12-04 12:49:11 -0500

    Update necessary creds

--- Alba Enhancer Chrome Extension/js/common/main.js
+++ Alba Enhancer Chrome Extension/js/common/main.js
@@ -37,7 +37,7 @@ Chart.defaults.global.devicePixelRatio = 10;
 
 $("ul.pull-right li.dropdown").last().find("a").first().find(".muted").hide(); // hide congregation name from interface
 var userName = $("ul.pull-right li.dropdown").last().find("a").first().find("span span").first().text().trim();
-var bingAPIKey = "AnLVuIekXyEvbagcmNli9pgb0bC6rBSbLoiIwqpG4YzxhnZII44HkTMwFN8bPEpI";
+var bingAPIKey = "AlHUPV2IJMYMwbz57Tz3ZhOvDtWzGTejTwQlZhG7i40cTQKhej4QXgfqtIkuc1qJ";
 var mapquestAPIKey = "cWthMOAPfdye1t02NcpkzD30NVLfhNy6";
 /*if (userName.includes("Olivier") || userName.includes("Grant")) { // allow admin menus
     var adminState = true;
@@ -46,68 +46,68 @@ var mapquestAPIKey = "cWthMOAPfdye1t02NcpkzD30NVLfhNy6";
 }*/
 var adminState = true; // everyone is an admin now!
 
-$("ul.nav:not(.pull-right) > li").each(function () {
-    $(this).find("span").text($(this).find("span").text().replace(String.fromCharCode(160) + String.fromCharCode(160) + "Users", "").replace(String.fromCharCode(160) + String.fromCharCode(160) + "Account", ""));
+$("ul.nav:not(.pull-right) > li").each(function() {
+  $(this).find("span").text($(this).find("span").text().replace(String.fromCharCode(160) + String.fromCharCode(160) + "Users", "").replace(String.fromCharCode(160) + String.fromCharCode(160) + "Account", ""));
 });
 
 function injectScript(content, id, tag) {
-    $("script#" + id).remove();
-    var node = document.getElementsByTagName(tag)[0];
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.innerHTML = content;
-    script.id = id;
-    node.appendChild(script);
+  $("script#" + id).remove();
+  var node = document.getElementsByTagName(tag)[0];
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.innerHTML = content;
+  script.id = id;
+  node.appendChild(script);
 }
 
 function setTextColorByLuma(rgb) {
-    var c = rgb.substring(1); // strip #
-    rgb = parseInt(c, 16); // convert rrggbb to decimal
-    var r = (rgb >> 16) & 0xff; // extract red
-    var g = (rgb >> 8) & 0xff; // extract green
-    var b = (rgb >> 0) & 0xff; // extract blue
+  var c = rgb.substring(1); // strip #
+  rgb = parseInt(c, 16); // convert rrggbb to decimal
+  var r = (rgb >> 16) & 0xff; // extract red
+  var g = (rgb >> 8) & 0xff; // extract green
+  var b = (rgb >> 0) & 0xff; // extract blue
 
-    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-    var output = "";
-    if (luma < 140) {
-        output = "#ffffff";
-    } else(
-        output = "#000000"
-    );
-    return output;
+  var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+  var output = "";
+  if (luma < 140) {
+    output = "#ffffff";
+  } else(
+    output = "#000000"
+  );
+  return output;
 }
 
 function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
-    return (rgb && rgb.length === 4) ? ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
-        ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
-        ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
 }
 
 function copyToClipboard(text) {
-    if (window.clipboardData && window.clipboardData.setData) {
-        return window.clipboardData.setData("Text", text);
+  if (window.clipboardData && window.clipboardData.setData) {
+    return window.clipboardData.setData("Text", text);
 
-    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
-        textarea.textContent = text;
-        textarea.style.position = "fixed";
-        document.body.appendChild(textarea);
-        textarea.select();
-        try {
-            return document.execCommand("copy");
-        } catch (ex) {
-            console.warn("Copy to clipboard failed.", ex);
-            return false;
-        } finally {
-            document.body.removeChild(textarea);
-        }
+  } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    textarea.style.position = "fixed";
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      return document.execCommand("copy");
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
     }
+  }
 }
 
 function orphanText(element) {
-  return element.contents().filter(function () {
-      return this.nodeType === 3;
+  return element.contents().filter(function() {
+    return this.nodeType === 3;
   }).text().trim();
 }
 

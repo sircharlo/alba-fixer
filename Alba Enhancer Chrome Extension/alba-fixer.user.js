c86c6f324f617636810a20a663e3e68ca0be7edd
Charles Olivier Savignac <sircharlo@gmail.com>
2022-08-29 16:45:28 -0400

    Re-enable mobile and send links

--- Alba Enhancer Chrome Extension/alba-fixer.user.js
+++ Alba Enhancer Chrome Extension/alba-fixer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Alba Enhancer
-// @version      0.1.17
+// @version      0.1.18
 // @description  Utilities and fixes for Alba
 // @author       SirCharlo
 // @match        https://www.mcmxiv.com/alba/*
@@ -288,8 +288,8 @@ $(function() {
         createShame();
       });
       $("#territories").on("click", "a.btn.btn-small.dropdown-toggle", function() {
-        //let isNotTel = !$(this).closest("tr").find("td.territory b").text().toLowerCase().includes("tel-");
-        $(this).closest("tr").find("a.cmd-open[rel*=mobile], a.cmd-send-to.both, a.cmd-send-to.print").parent().toggleClass("disabled", /*isNotTel*/ true);
+        let isNotTel = !$(this).closest("tr").find("td.territory b").text().toLowerCase().includes("tel-");
+        $(this).closest("tr").find("a.cmd-open[rel*=mobile], a.cmd-send-to.both, a.cmd-send-to.print").parent().toggleClass("disabled", true);
       });
     }
   } else if (curPage.includes("print") || curPage.includes("campaign")) {
